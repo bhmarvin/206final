d233f8e0fb5ac2cc44b4882874f57aea80bf2e39
Brent Marvin <bmarvin@umich.edu>
2023-12-12 09:07:54 -0500

    cleaned up/structured code

--- crash_details.py
+++ crash_details.py
@@ -1,25 +1,21 @@
 import requests
 import sqlite3
-from datetime import datetime
 
-# SQLite database connection
-conn = sqlite3.connect('proj_data.db')
-cursor = conn.cursor()
-
-# Create crashes table in the database
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS crash_details (
-        id INTEGER PRIMARY KEY,
-        drunk INTEGER,
-        weekday INTEGER, 
-        intersection_type TEXT,  
-        FOREIGN KEY (id) REFERENCES crashes(id)
-        UNIQUE(id) ON CONFLICT IGNORE
-    )
-''')
-conn.commit()
+def create_crash_details_table(cursor):
+    """Create crash_details table in the database if not exists"""
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS crash_details (
+            id INTEGER PRIMARY KEY,
+            drunk INTEGER,
+            weekday INTEGER, 
+            intersection_type TEXT,
+            FOREIGN KEY (id) REFERENCES crashes(id),
+            UNIQUE(id) ON CONFLICT IGNORE
+        )
+    ''')
 
 def get_case_details(state_case, case_year, state):
+    """Fetch case details from NHTSA DOT API"""
     base_url = "https://crashviewer.nhtsa.dot.gov/CrashAPI/crashes/GetCaseDetails"
     params = {
         "stateCase": state_case,
@@ -36,41 +32,52 @@ def get_case_details(state_case, case_year, state):
         print(f"Error: {response.status_code}")
         return None
 
-def fetch_and_insert_crashes(cursor, start_id):
-    # Make the API request with parameters for the entire timeframe
-    end_id = start_id + 25
-    for id in range(start_id,end_id):
+def fetch_and_insert_crash_details(cursor, start_id):
+    """Fetch and insert crash details into the database"""
+    # Make the API request with parameters for the next 25 crashes
+    end_id = start_id + 24
+    for id in range(start_id, end_id):
         cursor.execute("SELECT ST_Case, State, CrashDate FROM crashes WHERE id = ?", (id,))
         data = cursor.fetchone()
-        print(data)
+        
         if data is not None:
             st_case, state_code, date = data
             year = date.split('-')[0]
-            case_details = get_case_details(st_case,year,state_code)
+            case_details = get_case_details(st_case, year, state_code)
+            
             try:
                 results = case_details["Results"][0][0]
-            except IndexError as e:
+            except IndexError:
                 print(f"Case id {id} not found")
-                
+                continue
+
             if results is not None:
                 drunk = results['CrashResultSet']['DRUNK_DR']
-                intersection_type =results['CrashResultSet']['TYP_INTNAME']
-                #7 = saturday, 0 = sunday
+                intersection_type = results['CrashResultSet']['TYP_INTNAME']
+                # 7 = Saturday, 0 = Sunday
                 weekDay = results['CrashResultSet']['DAY_WEEK']
 
                 cursor.execute('''
                     INSERT OR IGNORE INTO crash_details VALUES (?, ?, ?, ?)
                 ''', (id, drunk, weekDay, intersection_type))
+
     with open('details_index.txt', 'w') as file:
         file.write(str(end_id))
     conn.commit()
 
+# SQLite database connection
+conn = sqlite3.connect('proj_data.db')
+cursor = conn.cursor()
 
-with open('details_index.txt', 'r') as file:
-    start_index = int(file.read().strip())
-fetch_and_insert_crashes(cursor,start_index)
-# Commit the changes to the database
+# Create crash_details table in the database if it doesn't exists
+create_crash_details_table(cursor)
 conn.commit()
 
-# Close the database connection
+# Read the start index from the file
+with open('details_index.txt', 'r') as file:
+    start_index = int(file.read().strip())
+
+# Fetch and insert the next 25 crash details
+fetch_and_insert_crash_details(cursor, start_index)
+conn.commit()
 conn.close()
